1a1c53cc8e147484f8090fc6086e598daa307c5e
Aden Herold <aden.herold1@gmail.com>
2019-10-19 13:12:59 +0930

    Added error handling to Mapbox location request and darksky weather request

--- app.js
+++ app.js
@@ -10,17 +10,29 @@ const weatherUrl = `https://api.darksky.net/forecast/${darkSkySecret}/${latLong}
 const locationUrl = `https://api.mapbox.com/geocoding/v5/mapbox.places/Los%20Angeles.json?access_token=${mapSecret}`;
 
 request({ url: locationUrl, json: true }, (err, res) => {
-  const { center } = res.body.features[0];
-  console.log(...center);
+  if (err) {
+    console.log("Unable to connect to Mapbox API");
+  } else if (!res.body.features.length) {
+    console.log("No location results found with the search term");
+  } else {
+    const { center } = res.body.features[0];
+    console.log(...center);
+  }
 });
 
 request({ url: weatherUrl, json: true }, (err, res) => {
-  const { timezone, currently, daily } = res.body;
-  const { temperature, precipProbability } = currently;
-  const { summary } = daily.data[0];
+  if (err) {
+    console.log("Unable to connect to DarkSky API");
+  } else if (res.body.error) {
+    console.log("Unable to find location");
+  } else {
+    const { timezone, currently, daily } = res.body;
+    const { temperature, precipProbability } = currently;
+    const { summary } = daily.data[0];
 
-  console.log(`Weather details for ${timezone}:`);
-  console.log(
-    `${summary} It is currently ${temperature} degrees out. There is a ${precipProbability}% chance of rain.`
-  );
+    console.log(`Weather details for ${timezone}:`);
+    console.log(
+      `${summary} It is currently ${temperature} degrees out. There is a ${precipProbability}% chance of rain.`
+    );
+  }
 });
