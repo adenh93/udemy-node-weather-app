e05135e7287cac1bbb6ca3849ad07a023c989634
Aden Herold <aden.herold1@gmail.com>
2019-10-19 10:37:59 +0930

    Accessed parsed json of darksky api response to print daily summary + current weather details. Added query params to exclude data blocks from response and change units to metric

--- app.js
+++ app.js
@@ -2,9 +2,17 @@ require("dotenv").config();
 const request = require("request");
 const secret = process.env.SECRET_KEY;
 
-const url = `https://api.darksky.net/forecast/${secret}/37.8267,-122.4233`;
+const latLong = "37.8267,-122.4233";
+const exclude = "minutely,hourly,alerts,flags";
+const url = `https://api.darksky.net/forecast/${secret}/${latLong}?exclude=${exclude}&units=si`;
 
-request({ url }, (err, res) => {
-  const { currently } = JSON.parse(res.body);
-  console.log(currently);
+request({ url, json: true }, (err, res) => {
+  const { timezone, currently, daily } = res.body;
+  const { temperature, precipProbability } = currently;
+  const { summary } = daily.data[0];
+
+  console.log(`Weather details for ${timezone}:`);
+  console.log(
+    `${summary} It is currently ${temperature} degrees out. There is a ${precipProbability}% chance of rain.`
+  );
 });
