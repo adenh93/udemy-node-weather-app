7d0086f9621b09eebccb3fd1a8fe01ff51fef02d
Aden Herold <aden.herold1@gmail.com>
2019-10-20 14:09:33 +0930

    Renamed views directory to templates, and configured express app to use the new directory to search for views

--- src/app.js
+++ src/app.js
@@ -2,10 +2,15 @@ require("dotenv").config();
 const path = require("path");
 const express = require("express");
 
+// Define paths for Express config
 const publicDir = path.join(__dirname, "../public");
+const viewsPath = path.join(__dirname, "../templates");
 
 const app = express();
+
+// Setup handlebars engine and views location
 app.set("view engine", "hbs");
+app.set("views", viewsPath);
 app.use(express.static(publicDir));
 
 app.get("", (req, res) => {
