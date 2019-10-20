f8edd254cdbf9575deee3fa703c31c6beb87bf40
Aden Herold <aden.herold1@gmail.com>
2019-10-20 13:55:38 +0930

    Added hbs templating engine package and created dynamic templates for about, help and index to replace static templates. Added about, help and index routes to render templates with data

--- src/app.js
+++ src/app.js
@@ -5,7 +5,29 @@ const express = require("express");
 const publicDir = path.join(__dirname, "../public");
 
 const app = express();
+app.set("view engine", "hbs");
 app.use(express.static(publicDir));
+
+app.get("", (req, res) => {
+  res.render("index", {
+    title: "Weather",
+    name: "Aden Herold"
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", {
+    title: "About",
+    name: "Aden Herold"
+  });
+});
+
+app.get("/help", (req, res) => {
+  res.render("help", {
+    title: "Help",
+    help: "This is a help message."
+  });
+});
 
 app.get("/weather", (req, res) => {
   res.send({ location: "Melbourne, Victoria, Australia", forecast: "Sunny" });
