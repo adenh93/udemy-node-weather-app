13c2fa66d962b09828983ca407a7f58c73649c83
Aden Herold <aden.herold1@gmail.com>
2019-10-21 16:36:01 +0930

    Added call to fetch in app.js to get forecast data from backend weather endpoint with a predetermined 'address' query parameter  and log to the console

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,10 @@
 console.log("Client side javascript file has loaded");
+
+fetch("http://localhost:3000/weather?address=darwin").then(response => {
+  response.json().then(({ error, forecast, location }) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log(`${location}\n${forecast}`);
+  });
+});
